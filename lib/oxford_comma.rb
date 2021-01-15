4ea53239d0646fa827324de434a589939529fc50
James <james.michalec58@gmail.com>
2021-01-15 12:07:48 -0800

    commit tree ce63065cb8166bf0281aa1e16c0c2975e674bf3b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def oxford_comma(array)
+
+    if array.size == 1
+        array[0]
+    elsif array.size == 2
+        joined_string = ""
+        joined_string = array.join(" and ")
+        joined_string
+    elsif array.size >= 3
+        last_elemenet = array.last 
+        array.pop 
+        joined = array.join(", ")
+        last_phrase = ", and #{last_elemenet}"
+        joined + last_phrase
+        
+    end
 
 end
